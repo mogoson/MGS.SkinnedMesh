d643424fbcd7e691f72824cf141628a3e5650f2a
Mogoson <mogoson@qq.com>
2018-04-05 17:31:45 +0800

    Modify GenericEditor.

--- Assets/MGS-FlexiblePipe/Editor/SkinEditor.cs
+++ Assets/MGS-FlexiblePipe/Editor/SkinEditor.cs
@@ -10,7 +10,7 @@
  *  Description  :  Initial development version.
  *************************************************************************/
 
-using Mogoson.EditorExtension;
+using Mogoson.UEditor;
 using UnityEditor;
 using UnityEngine;
 
